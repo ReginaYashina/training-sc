53385e294c1659427b2d109c9988b3952a8ec8c5
Regina Yashina <reginayashina@inbox.ru>
2024-07-31 21:21:03 +0300

    fix: fix component Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import myImg from './myImg.png';
+
 import './App.css';
-import { Card } from './components/Card';
-import { Image } from './components/Image';
-import { Title } from './components/Title';
-import { Text } from './components/Text';
-import { Button } from './components/Button';
+import Card from './components/Card';
 import { Wrapper } from './components/Wrapper';
 
 function App() {
   return (
     <div className='App'>
-      <Wrapper color={'#c2c2c2'}>
-        <Card>
-          <Image src={myImg}></Image>
-          <Title>Headline</Title>
-          <Text>
-            Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
-            ornare in venen.
-          </Text>
-          <Button buttonType={'primary'}>See more</Button>
-          <Button buttonType={'outlined'}>Save</Button>
-        </Card>
-        <Card>
-          <Image src={myImg}></Image>
-          <Title>Headline</Title>
-          <Text>
-            Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
-            ornare in venen.
-          </Text>
-          <Button buttonType={'primary'}>See more</Button>
-          <Button buttonType={'outlined'}>Save</Button>
-        </Card>
-        <Card>
-          <Image src={myImg}></Image>
-          <Title>Headline</Title>
-          <Text>
-            Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
-            ornare in venen.
-          </Text>
-          <Button buttonType={'primary'}>See more</Button>
-          <Button buttonType={'outlined'}>Save</Button>
-        </Card>
+      <Wrapper color='#ccc'>
+        <Card />
+        <Card />
+        <Card />
       </Wrapper>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from './logo.svg';
+import myImg from '../myImg.png';
+import { Wrapper } from './Wrapper';
+import { Title } from './Title';
+import { Button } from './Button';
+import { Text } from './Text';
+import { Image } from './Image';
 
-export const Card = styled.div`
+const Card = () => {
+  return (
+    <StyledCard>
+      <Image src={myImg} />
+      <Title>Headline</Title>
+      <Text>
+        Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
+        ornare in venen.
+      </Text>
+      <Button buttonType={'primary'}>See more</Button>
+      <Button buttonType={'outlined'}>Save</Button>
+    </StyledCard>
+  );
+};
+
+export default Card;
+
+export const StyledCard = styled.article`
   border-radius: 15px;
   padding: 10px;
   box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
@@ -9,3 +33,5 @@ export const Card = styled.div`
   max-width: 300px;
   min-height: 350px;
 `;
+
+// вывести в App заголовок,  описание и путь к картинке
